f41b45370094adfbb0ba45afb616d63c11b975f7
LuK1337 <priv.luk@gmail.com>
2018-05-15 15:21:41 +0200

    helium: Set vendor security patch level
     * Matches patch level from MIUI 8.8.23 Global dev release
    
    Change-Id: I5e492dfb8210932ac4883bf2eb4bd749b373d1a9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,5 +53,8 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 # Tap-to-Wake
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2018-07-01
+
 # inherit from the proprietary version
 -include vendor/xiaomi/helium/BoardConfigVendor.mk
