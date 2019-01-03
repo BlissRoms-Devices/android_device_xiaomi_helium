2808c5e12d6332b42466ce9c9461d36e80b86ee8
TARKZiM <tom8476oo@gmail.com>
2019-01-03 20:14:46 +0800

    helium: update build fingerprint to V10.1.1.0.NBDMIFI
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss_helium.mk
+++ bliss_helium.mk
@@ -33,5 +33,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Use the latest approved GMS identifiers unless running a signed build
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="helium-user 7.0 NRD90M V9.6.2.0.NBDMIFD release-keys"
-BUILD_FINGERPRINT=Xiaomi/helium/helium:7.0/NRD90M/V9.6.2.0.NBDMIFD:user/release-keys
+    PRIVATE_BUILD_DESC="helium-user 7.0 NRD90M V10.1.1.0.NBDMIFI release-keys"
+    BUILD_FINGERPRINT=Xiaomi/helium/helium:7.0/NRD90M/V10.1.1.0.NBDMIFI:user/release-keys
